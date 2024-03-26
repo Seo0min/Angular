2752c8e4c0394de26bb2b25b593175d65d160a71
Seoyeong Min <mseo1123@naver.com>
2024-03-26 22:30:55 +0100

    Add the new component

---
+++ src/app/server/server-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  //   should be unique
+  templateUrl: './server-component.html',
+  // point to the html file
+})
+export class ServerComponent {}
